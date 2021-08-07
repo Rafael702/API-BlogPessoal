a229b0e81372a50451859f1253310eed24e0ad2f
Rafael <rafaeldealmeida.192001@gmail.com>
2021-08-07 14:17:53 -0300

    Implementação do Consumo de API em typeScript - Implementaçã da Service.

--- blogpessoal/src/app/app.module.ts
+++ blogpessoal/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +19,8 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
